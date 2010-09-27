7830c17b598f05df21ad29ce37c21a707f3711bb
Sebastian Bauer <gjlnetwork@googlemail.com>
2010-09-27 09:49:02 +0000

    GZiP update

--- ircbot.php
+++ ircbot.php
@@ -1,11 +1,13 @@
 <?php
 
+include_once('config.php');
+
 echo 'Connecting to tiscali.dk.quakenet.org'.PHP_EOL;
 $conn = fsockopen('tiscali.dk.quakenet.org', 6667);
 echo 'Connected'.PHP_EOL;
 
-sendCommand("USER octrinbot 0 0 octrin", $conn, false);
-sendCommand("NICK octrinbot", $conn);
+sendCommand("USER ".USERNAME." 0 0 ".USERNAME, $conn, false);
+sendCommand("NICK ".USERNAME, $conn);
 $connected = false;
 $time = time();
 while(!feof($conn)){
@@ -26,15 +28,15 @@ while(!feof($conn)){
         }
     }
     if(strpos($result, "JOIN #octrin")!==false && !$connected){
-        sendCommand("AUTH octbot secretpass", $conn);
+        sendCommand("AUTH ".USERNAME." ".PASSWORD, $conn);
         sendCommand("PRIVMSG #octrin :lolol i'm in", $conn);
         $connected = true;
     }
     if($connected && (time()-$time)>60*5) {
         $time = time();
-        
-        $hosts = unserialize(file_get_contents("http://www.lfsworld.net/pubstat/get_stat2.php?version=1.4&idk=8R1y661kN71KgPB228B155Z0Q13ZEYke&action=hosts&s=2"));
-        
+
+        $hosts = unserialize(gzdecode(file_get_contents("http://www.lfsworld.net/pubstat/get_stat2.php?version=1.4&idk=2FDVRzY1n7Xp3Vp93jSbHus4rtFMWvF2&action=hosts&s=2&c=1")));
+
         foreach($hosts as $host){
             if(strpos(strtolower($host['hostname']), 'octrin')===false){
                 continue;
